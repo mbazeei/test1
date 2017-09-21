bb8e084f487cae058bddce8ac4084cf988fff0d6
SDB Team <sdbmobileteam@SDBs-iMac.local>
2017-09-21 09:30:59 +0300

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by SDB Mobile Team on 9/21/17.
+//  Copyright © 2017 SDB Mobile Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
